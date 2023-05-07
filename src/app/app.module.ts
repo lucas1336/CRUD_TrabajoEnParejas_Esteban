0ae62fde05e5dbe2348da340c6ff4b5e11b09d37
Lucas Ruiz <85413494+U202110085@users.noreply.github.com>
2023-05-07 11:32:51 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { TableComponent } from './components/table/table.component';
+import { NewComponent } from './components/new/new.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomeComponent,
+    TableComponent,
+    NewComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
