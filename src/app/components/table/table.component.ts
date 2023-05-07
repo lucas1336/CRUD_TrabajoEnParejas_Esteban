fa00e64172e11b0b4fe3b8887c7ee96f69a24b86
Lucas Ruiz <85413494+U202110085@users.noreply.github.com>
2023-05-07 13:14:01 -0500

    feat: new component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
-
+  openNewComponent() {}
 }
